99580522892687e111ec01c54ddeaccd087760b8
NoctalIvan <sedletzk@gmail.com>
2018-12-19 01:32:26 +0100

    stock dashboard - tv connection

---
+++ front/components/tdv.js
@@ -0,0 +1,27 @@
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_01312"></div>
+  <div class="tradingview-widget-copyright"><a href="https://fr.tradingview.com/symbols/AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL graphique</span></a> par TradingView</div>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "AAPL",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "Light",
+  "style": "1",
+  "locale": "fr",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "studies": [
+    "RSI@tv-basicstudies"
+  ],
+  "container_id": "tradingview_01312"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
